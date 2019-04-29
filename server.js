0d7d19d693b76386f256bc53143be52fb1aa4355
Gary Anderson <ganderson@seattleacademy.org>
2019-04-29 09:15:22 -0700

    Add index.html for servering tests

--- server.js
+++ server.js
@@ -6,28 +6,12 @@ const port = 1206
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.use(express.static('public'));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
-
 var gsjson = require('google-spreadsheet-to-json');
-
-
-
-
-
-gsjson({
-        spreadsheetId: '1omktI230oCNJiY1_ckgyiqFxbs-_uRpWWoagOjJBKMM',
-        // other options...
-    })
-    .then(function(result) {
-        console.log(result.length);
-        console.log(result);
-    })
-    .catch(function(err) {
-        console.log(err.message);
-        console.log(err.stack);
-    });
 
 app.get('/json', (req, res) => {
     
